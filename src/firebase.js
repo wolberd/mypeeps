0479dbe62547ec7b534f64bd5316c836b1f3f48a
David Wolber <wolberd@usfca.edu>
2025-03-27 14:05:21 -0700

    .env security

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0aW0gtuzEJw2dT9lp6EevZibbws2FkNg",
-  authDomain: "peopledb-e16e9.firebaseapp.com",
-  projectId: "peopledb-e16e9",
-  storageBucket: "peopledb-e16e9.firebasestorage.app",
-  messagingSenderId: "636099872924",
-  appId: "1:636099872924:web:e5dedea483ecb9eac14559"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
